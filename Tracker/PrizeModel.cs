65cc8f9013577fbbe13d03cc22e2fd113d747a4b
JohnMorris4 <johnmorris@morrisje.com>
2018-05-04 09:28:27 -0400

    Finished forms started wiring up the data and logic

--- Tracker/PrizeModel.cs
+++ Tracker/PrizeModel.cs
@@ -3,21 +3,42 @@
     public class PrizeModel
     {
         /// <summary>
+        /// Set the unique Id for the prize.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
         /// Initializes hte placenumber of of the prize
         /// </summary>
         public int PlaceNumber { get; set; }
+
         /// <summary>
         /// initializes the place name
         /// </summary>
         public string PlaceName { get; set; }
+
         /// <summary>
         /// Creates the Prize amount vs percentage
         /// </summary>
         public decimal PrizeAmount { get; set; }
+
         /// <summary>
         /// percentage vs the amount
         /// </summary>
         public double PrizePercentage { get; set; }
 
+        public PrizeModel()
+        {
+
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            //int placeNumberValue 
+        }
     }
+
 }
--- TrackerUI/CreatePrizeForm.Designer.cs
+++ TrackerUI/CreatePrizeForm.Designer.cs
@@ -150,6 +150,7 @@
             this.createPrizeButton.TabIndex = 26;
             this.createPrizeButton.Text = "Create Prize";
             this.createPrizeButton.UseVisualStyleBackColor = true;
+            this.createPrizeButton.Click += new System.EventHandler(this.createPrizeButton_Click);
             // 
             // CreatePrizeForm
             // 
